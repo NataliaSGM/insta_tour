713a6b5f2a4805e10457afd7d6049edb00583d5c
natalia-st <nastgm55@gmail.com>
2023-11-29 13:12:59 +0100

    second commit

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
+  const isAuthenticated = false;
+
+  return ( 
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ): (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img 
+            src="https://images.unsplash.com/photo-1509136561942-7d8663edaaa2?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="logo"
+            className="xs:hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+          
+        </>
+      )}
+    </>
   )
 }
 
