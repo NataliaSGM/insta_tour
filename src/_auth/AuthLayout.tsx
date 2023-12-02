5d2a21e11e9c6ea75ded1df399340033352cf57a
natalia-st <nastgm55@gmail.com>
2023-12-02 11:38:45 +0100

    Third commit

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -15,7 +15,7 @@ const AuthLayout = () => {
           <img 
             src="https://images.unsplash.com/photo-1509136561942-7d8663edaaa2?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="logo"
-            className="xs:hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            className="sm:hidden lg:block h-screen w-1/2 object-cover bg-no-repeat"
           />
           
         </>
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,11 +1,13 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Link } from "react-router-dom"
 
 import {
- Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
+ Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import  Loader  from "@/components/shared/Loader"
 import { useForm } from "react-hook-form"
 import { SignupValidation } from "@/lib/validation"
 import { z } from "zod"
@@ -13,6 +15,8 @@ import { z } from "zod"
 
 
 const SignupForm = () => {  
+const isLoading = false
+
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
@@ -23,11 +27,9 @@ const SignupForm = () => {
     },
   })
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    const newUser = await createUserAccount(values)
+    
   }
 
   return (
@@ -35,28 +37,89 @@ const SignupForm = () => {
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
+        <h2 className="h3-bold md:h2-bold pt5 sm:pt-12">Create new account</h2>
+        <p className="text-light-3 small-medium md:base-regular">To use instaTour enter your account details</p>
       
-      </div>
     
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="user name" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-3 w-100 mt-4" >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" className="shad-input" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="shad-button_primary">
+        {isLoading ? (
+          <div className="flex-center gap-2">
+            <Loader />Loading...
+          </div>
+        ) : "Sign Up" }
+        </Button>
+        <p className="text-small-regular text-light-2 text-center mt-2">
+          Already have an account?
+          <Link to="/sign-in" className="text-primary-500
+          text-small-semibold ml-1">Login</Link>
+        </p>
+      </form>
+      </div>
   </Form>
   
   
